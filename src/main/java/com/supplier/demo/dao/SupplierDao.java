ac7f2cf60d2140faf6292203dd7c0a4b5048679f
lulu <lulupiko0347@gmail.com>
2023-06-18 21:53:52 +0800

    增加介面方法註解

--- src/main/java/com/supplier/demo/dao/SupplierDao.java
+++ src/main/java/com/supplier/demo/dao/SupplierDao.java
@@ -9,21 +9,21 @@ public interface SupplierDao {
 
 	List<Supplier> getSuppliers();//取得所有供應商
 	
-	Supplier getSupplierById(Integer supplierId);
+	Supplier getSupplierById(Integer supplierId);//以供應商ID取得
 	
-	Supplier getSupplierByAccount_target(String account);
+	Supplier getSupplierByAccount_target(String account);//以供應商帳號取得 用於驗證是否與資料庫重複
 	
-	Supplier getSupplierByName_target(String name);
+	Supplier getSupplierByName_target(String name);//以供應商姓名取得 用於驗證是否與資料庫重複
 	
-	List<Supplier> getSupplierByAccount(String account);
+	List<Supplier> getSupplierByAccount(String account);//以供應商帳號取得 用於模糊查詢
 	
-	List<Supplier> getSupplierByName(String name);
+	List<Supplier> getSupplierByName(String name);//以供應商姓名取得 用於模糊查詢
 	
-	List<Supplier> getSupplierByTimeRange(String startTime, String endTime);
+	List<Supplier> getSupplierByTimeRange(String startTime, String endTime);//以供應商新增時間取得
 	
-	Integer createSupplier(SupplierRequest supplierRequest);
+	Integer createSupplier(SupplierRequest supplierRequest);//新增供應商
 	
-	void updateSupplier(SupplierRequest supplierRequest, Integer supplierId);
+	void updateSupplier(SupplierRequest supplierRequest, Integer supplierId);//修改供應商
 	
-	void deleteSupplierById(Integer supplierId);
+	void deleteSupplierById(Integer supplierId);//刪除供應商
 }
